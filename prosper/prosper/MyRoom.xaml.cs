321c6f4bc12f34767cd7b29f1265e17723335e36
Emily Entwistle <emily-entwistle@hotmail.com>
2018-03-08 15:47:22 +1000

    Added stage popups, Lobster font

--- prosper/prosper/MyRoom.xaml.cs
+++ prosper/prosper/MyRoom.xaml.cs
@@ -17,6 +17,9 @@ namespace prosper
 		{
             InitializeComponent ();
             game = g;
+            //place character on screen
+                //TODO
+
             if (!game.gameInitialised)
             {
                 TutorialPopup.IsVisible = true;
@@ -56,15 +59,55 @@ namespace prosper
             TutorialPopup.IsVisible = false;
             //temporary
             DisplayAlert("Tutorial", "...awaiting implementation", "OK");
-            // now initialise stage -- check first
-                //TODO
+            if (!game.stageInitialised)
+            {
+                InitialiseStage();
+            }
+
+        }
+        void InitialiseStage()
+        {
+
+            if (game.stage == Game.Stage.One)
+            {
+                StagePopupTitle.Text = "Stage 1";
+                StagePopupText.Text = "The objective of stage 1 is to save $100 to purchase a mobile phone." +
+                    "\nYou can earn money from doing chores for pocket money." +
+                    "\nTo avoid running low on happiness you can buy items from the shop.";
+                StageBeginText.Text = "Begin Stage 1!";
+            }
+            else if (game.stage == Game.Stage.Two)
+            {
+                StagePopupTitle.Text = "Stage 2";
+                StagePopupText.Text = "The objective of stage 2 is to save $1000 to purchase a car." +
+                    "\nYou can now earn money faster from working different jobs." +
+                    "\nYou will now pay extra bills per round for your phone";
+                StageBeginText.Text = "Begin Stage 2!";
+            }
+            else if (game.stage == Game.Stage.Three)
+            {
+                StagePopupTitle.Text = "Stage 3";
+                StagePopupText.Text = "The objective of stage 3 is to save $5000 to purchase a holiday." +
+                    "\nYou can now also earn money through investing - see Manage $." +
+                    "\nYou will now pay extra bills per round for your car.";
+                StageBeginText.Text = "Begin Stage 3!";
+            }
+            StagePopup.IsVisible = true;
         }
         void OnSkipTutorial()
         {
             game.gameInitialised = true;
             TutorialPopup.IsVisible = false;
-            //now initialise stage -- check first
-                //TODO
+            if (!game.stageInitialised)
+            {
+                InitialiseStage();
+            }
         }
+        void OnStageBegin()
+        {
+            game.stageInitialised = true;
+            StagePopup.IsVisible = false;
+        }
+
     }
 }
